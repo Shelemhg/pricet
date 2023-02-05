5ac0ecff567d1bff1db5afb81fd618347717d85d
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-02-05 01:09:32 -0600

    Installed Swagger

---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+info: {
+	title: 'PriceT',
+	description: 'Description',
+},
+host: 'pricet.onrender.com',
+schemes: ['https']
+};
+
+// const outputFile = './path/swagger-output.json';
+// const endpointsFiles = ['./path/endpointsUser.js', './path/endpointsBook.js'];
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./app.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+'endpointsFiles' only the root file where the route starts,
+such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
