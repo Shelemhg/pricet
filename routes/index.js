10546a0aa44164813696103e8b9413999265f39b
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-02-05 01:10:29 -0600

    Added Swagger.js to intex

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 //ADDED SWAGGER ROUTES
-// router.use('/', require('./swagger'));
+router.use('/', require('./swagger'));
 router.use('/amazon', require('./amazon'));
 
 module.exports = router;
