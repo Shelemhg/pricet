561bd8de11a245abf36948104249e88011a73887
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-02-04 22:13:32 -0600

    Started node server

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+// const router = require('./routes');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app
+.use(bodyParser.json())
+.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
+.use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+if (err) {
+    console.log(err);
+} else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+}
+});
