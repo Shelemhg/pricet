a02aa9cf71dfedf1b72849e58fd360c8f45fc560
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-02-25 18:07:01 -0600

    Updated callbackURL

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
 		clientID:     process.env.GOOGLE_CLIENT_ID,
 		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		callbackURL: "http://localhost:3000/google/callback",
+		callbackURL: "https://pricet.onrender.com/google/callback",
 		passReqToCallback   : true
 	},
 	function(request, accessToken, refreshToken, profile, done) {
