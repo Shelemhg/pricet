3b3c71e97f35ade1a6fe3faafaadae3d31a3cb4a
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-02-25 23:46:19 -0600

    Created home page and added send static page home

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
-router.use('/', require('./googleAuth'));
+// Working googleOAuth2.0
+router.use('/login', require('./googleAuth'));
+
+
 //ADDED SWAGGER ROUTES
 router.use('/', require('./swagger'));
 router.use('/amazon', require('./amazon'));
+
+// router.use(express.static(path.join(__dirname, '/public')));
+// router.use(express.static(path.resolve(__dirname, "public")));
+router.use(express.static('public'));
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views/index.html'));
+  });
+
 
 
 module.exports = router;
